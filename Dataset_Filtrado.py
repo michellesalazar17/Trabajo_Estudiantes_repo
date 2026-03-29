568b82e070839afcc794e743ef2bfdb822c75ec1
Aleegr23 <alejandrogr2025@gmail.com>
2026-03-29 17:25:42 +0000

    cambios en como se va filtrar el dataset

--- Dataset_Filtrado.py
+++ Dataset_Filtrado.py
@@ -11,12 +11,18 @@ print(df.columns)
 #filtrar nivel academico
 df_2 = df[df["Academic_Level"] == "Graduate"]
 
-print(df_2)
+#cuantos estudiantes hay por pais
+conteo = df_2.groupby("Country").size()
 
-#seleccionar los paises que vamos a usar
-paises = ["India", "USA", "UK", "Australia", "Bangladesh"]
+print(conteo)
 
-df_3 = df_2[df_2["Country"].isin(paises)]
+#paises con mas de 15 estudiantes
+resumen_paises = conteo[conteo > 15].index
+
+print(resumen_paises)
+
+#filtramos el dataset para que solo sean de esos paises
+df_3 = df_2[df_2["Country"].isin(resumen_paises)]
 
 print(df_3)
 
@@ -24,6 +30,13 @@ print(df_3)
 df_filtrado = df_3.drop(["Student_ID", "Age", "Relationship_Status", "Gender"], axis=1)
 
 print(df_filtrado)
+
+#los paises que quedaron
+paises_finales = df_filtrado["Country"].unique()
+
+print(paises_finales)
+
+
 
 
 
